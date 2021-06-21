8b277f278a2d9cbb47a6b9897b13da82ad55fcb7
unes <anoscasillas@gmail.com>
2021-06-21 15:23:20 +0100

    excel to json v1.0.1

--- app.js
+++ app.js
@@ -1,21 +1,80 @@
 const express = require('express'),
-    xlsxtojson = require("xlsx-to-json");
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
     xlstojson = require("xls-to-json"),
+    xlsxtojson = require("xlsx-to-json"),
     app = express();
 
-app.post('/api/xlstojson', function(req, res) {
-    xlsxtojson({
-        input: "./client.xlsx",  // input xls
-        output: "client.json", // output json
-    }, function(err, result) {
-        if(err) {
-          res.json(err);
-        } else {
-          res.json(result);
-        }
-    });
+app.use(bodyParser.json());
+
+//multers Config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/')
+    },
+    filename: (req, file, cb) => {
+        let datetimestamp = Date.now();
+        let fileNameCustm = file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]
+        cb(null, fileNameCustm)
+    }
 });
-const port = 3030;
-app.listen(port, function () {
-    console.log("SERVER I'M HERE IN " + port)
-})
+
+const upload = multer({
+    storage: storage,
+    // filter
+    fileFilter: (req, file, callback) => {
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length - 1]) === -1) {
+            return callback(new Error('Wrong file type'));
+        }
+        callback(null, true);
+    }
+}).single('file');
+
+// Post: excel to json
+app.post('/xlstojson', (req, res) => {
+    let exceltojson;
+    upload(req, res, (err) => {
+        if (err) {
+            res.json({ error_code: 1, err_desc: err });
+            return;
+        }
+        // Check file from multer object
+        if (!req.file) {
+            res.json({ error_code: 1, err_desc: "No file passed" });
+            return;
+        }
+
+        // Chech the file type "excel"        
+        if (req.file.originalname.split('.')[req.file.originalname.split('.').length - 1] === 'xlsx') {
+            exceltojson = xlsxtojson;
+        } else {
+            exceltojson = xlstojson;
+        }
+        console.log(req.file.path);
+
+        // Response
+        try {
+            let jsonName = req.file.originalname.split('.')[0];
+            exceltojson({
+                input: req.file.path,
+                output: `./jsonfiles/${jsonName}.json`,
+            }, (err, result) => {
+                if (err) {
+                    return res.json({ error_code: 1, err_desc: err, data: null });
+                }
+                res.json({data: result });
+            });
+        } catch (e) {
+            res.json({ error_code: 1, err_desc: "excel file Corupted" });
+        }
+    })
+
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen('3030', () => {
+    console.log('3030: At your service, sir...');
+});
